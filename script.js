ede0445a026e1071db9e68afe7a8f43ca34ca8ad
Nikhil <nikhilsharma26500@outlook.com>
2023-09-15 10:49:28 +0530

    fixed carousel and footer

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// Navbar
-// function Menu(e){
-//   let list = document.querySelector('ul');
-//   e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
-// }
-
-
-// Carousel
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    navText: [
-      "<i class='fa fa-caret-left'></i>",
-      "<i class='fa fa-caret-right'></i>"
-    ],
-    autoplay: true,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 5
-      }
-    }
-  })
+export function toggleScheduleBox() {
+  var scheduleBox = document.getElementById("schedule_box");
+  if (scheduleBox.style.display === "none") {
+    scheduleBox.style.display = "block";
+  } else {
+    scheduleBox.style.display = "none";
+  }
+}
